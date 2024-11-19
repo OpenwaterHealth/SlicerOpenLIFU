fd0c5545fa97e785572294e7c0b351d0b7a3c633
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-11-19 15:18:48 -0500

    Integrate virtual fitting (#139)

--- OpenLIFULib/OpenLIFULib/transducer.py
+++ OpenLIFULib/OpenLIFULib/transducer.py
@@ -1,5 +1,6 @@
 from typing import Optional, TYPE_CHECKING, Callable, Any
 import numpy as np
+import vtk
 import slicer
 from slicer import (
     vtkMRMLModelNode,
@@ -16,6 +17,17 @@ from OpenLIFULib.coordinate_system_utils import (
 
 if TYPE_CHECKING:
     import openlifu # This import is deferred at runtime, but it is done here for IDE and static analysis purposes
+
+def create_openlifu2slicer_matrix(transducer : "openlifu.Transducer") -> np.ndarray:
+    """
+    Returns a 4x4 affine transform matrix that maps LPS points in transducer units to RAS points in mm
+    """
+    # TODO: Instead of harcoding 'LPS' here, use something like a "dims" attribute that should be associated with
+    # the `transducer` object. There is no such attribute yet but it should exist eventually once this is done:
+    # https://github.com/OpenwaterHealth/opw_neuromod_sw/issues/3
+    return linear_to_affine(
+        get_xxx2ras_matrix('LPS') * get_xx2mm_scale_factor(transducer.units)
+    )
 
 @parameterPack
 class SlicerOpenLIFUTransducer:
@@ -49,12 +61,7 @@ class SlicerOpenLIFUTransducer:
         transform_node.SetName(slicer.mrmlScene.GenerateUniqueName(f"{transducer.id}-matrix"))
         model_node.SetAndObserveTransformNodeID(transform_node.GetID())
 
-        # TODO: Instead of harcoding 'LPS' here, use something like a "dims" attribute that should be associated with
-        # the `transducer` object. There is no such attribute yet but it should exist eventually once this is done:
-        # https://github.com/OpenwaterHealth/opw_neuromod_sw/issues/3
-        openlifu2slicer_matrix = linear_to_affine(
-            get_xxx2ras_matrix('LPS') * get_xx2mm_scale_factor(transducer.units)
-        )
+        openlifu2slicer_matrix = create_openlifu2slicer_matrix(transducer)
         if transducer_matrix is None:
             transducer_matrix = np.eye(4)
         if transducer_matrix_units is None:
@@ -69,6 +76,37 @@ class SlicerOpenLIFUTransducer:
         return SlicerOpenLIFUTransducer(
             SlicerOpenLIFUTransducerWrapper(transducer), model_node, transform_node
         )
+
+    def update_transform(self, transform_matrix:np.ndarray, transform_matrix_units:Optional[str]=None):
+        """ Update the transducer transform by postcomposing an additional matrix on top of the current transform.
+
+        The transform_matrix is assumed to be in "openlifu" style transducer coordinates, which is currently hardcoded to being LPS,
+        so this function does the needed conversions.
+
+        This function is useful for applying transform updates that come from algorithms in openlifu-python,
+        where the transform would be in openlifu conventions.
+        """
+
+        # Convert transform matrix from whaetver units it came with into transducer units
+        if transform_matrix_units is None:
+            transform_matrix_units = self.transducer.transducer.units
+        transform_in_native_transducer_coordinates = self.transducer.transducer.convert_transform(transform_matrix, transform_matrix_units)
+
+        # Get the current transform matrix, as a mapping from transducer-space-and-units to slicer RAS space and mm
+        current_transform_vtk = vtk.vtkMatrix4x4()
+        self.transform_node.GetMatrixTransformToParent(current_transform_vtk)
+        current_transform = slicer.util.arrayFromVTKMatrix(current_transform_vtk)
+
+        # Get the converstions back and forth between LPS-with-transducer-units and RAS-with-mm
+        openlifu2slicer_matrix = create_openlifu2slicer_matrix(self.transducer.transducer)
+        slicer2openlifu_matrix = np.linalg.inv(openlifu2slicer_matrix)
+
+        # Compute the new transform by postcomposing the new transform with the current transform
+        new_transform = openlifu2slicer_matrix @ transform_in_native_transducer_coordinates @ slicer2openlifu_matrix @ current_transform
+        new_transform_vtk = numpy_to_vtk_4x4(new_transform)
+        self.transform_node.SetMatrixTransformToParent(new_transform_vtk)
+
+
 
     def clear_nodes(self) -> None:
         """Clear associated mrml nodes from the scene. Do this when removing a transducer."""
