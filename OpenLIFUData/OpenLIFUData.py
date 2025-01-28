177b3f2f420b8a6633335ea136f189ae3e545bfa
Sadhana Ravikumar <sadhana.ravikumar@kitware.com>
2025-01-28 13:55:50 -0500

    Clear additional model nodes and folder upon transducer deletion (#151)

--- OpenLIFUData/OpenLIFUData.py
+++ OpenLIFUData/OpenLIFUData.py
@@ -547,6 +547,7 @@ class OpenLIFUDataWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
         # This ensures that we properly handle SlicerOpenLIFU objects that become invalid when their nodes are deleted
         self.addObserver(slicer.mrmlScene, slicer.vtkMRMLScene.NodeAboutToBeRemovedEvent, self.onNodeAboutToBeRemoved)
+        self.addObserver(slicer.mrmlScene, slicer.vtkMRMLScene.NodeAboutToBeRemovedEvent, self.onNodeAboutToBeRemoved)
         self.addObserver(slicer.mrmlScene, slicer.vtkMRMLScene.NodeAddedEvent, self.onNodeAdded)
         self.addObserver(slicer.mrmlScene, slicer.vtkMRMLScene.NodeRemovedEvent, self.onNodeRemoved)
 
@@ -1600,6 +1601,7 @@ class OpenLIFUDataLogic(ScriptedLoadableModuleLogic):
             affiliated_node_attribute_name: List of the possible names of the affected vtkMRMLNode-valued SlicerOpenLIFUTransducerNode attribute
                 (so "transform_node" or "model_node" or "body_model_node" or "surface_model_node")
         """
+        
         matching_transducer_openlifu_ids = [
             transducer_openlifu_id
             for transducer_openlifu_id, transducer in self.getParameterNode().loaded_transducers.items()
@@ -1612,6 +1614,9 @@ class OpenLIFUDataLogic(ScriptedLoadableModuleLogic):
         assert(len(matching_transducer_openlifu_ids) <= 1)
 
         if matching_transducer_openlifu_ids:
+            # Remove the transducer, but keep any other nodes under it. This transducer was removed
+            # by manual mrml scene manipulation, so we don't want to pull other nodes out from
+            # under the user.
             # Remove the transducer, but keep any other nodes under it. This transducer was removed
             # by manual mrml scene manipulation, so we don't want to pull other nodes out from
             # under the user.
