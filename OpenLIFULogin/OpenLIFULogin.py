bada8883badf4ce1d025df0d6808adb42cc5a0fe
Andrew Howe <arhowe00@gmail.com>
2025-02-10 15:39:44 -0500

    Add empty login module (#174)
    Closes #174
    
    - Add a single button to the module for toggling user account mode, as
      well as some utilities for getting the user account mode state from
      the parameter node of the login module.

---
+++ OpenLIFULogin/OpenLIFULogin.py
@@ -0,0 +1,208 @@
+from typing import Optional, TYPE_CHECKING
+import json
+
+from OpenLIFULib.user_account_mode_util import set_user_account_mode_state
+import qt
+
+import vtk
+import numpy as np
+
+import slicer
+from slicer.i18n import tr as _
+from slicer.i18n import translate
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+
+from slicer.parameterNodeWrapper import parameterNodeWrapper
+from slicer import (
+    vtkMRMLScriptedModuleNode,
+)
+
+from OpenLIFULib import (
+    openlifu_lz,
+)
+
+if TYPE_CHECKING:
+    import openlifu # This import is deferred at runtime using openlifu_lz, but it is done here for IDE and static analysis purposes
+    import openlifu.db
+
+#
+# OpenLIFULogin
+#
+
+class OpenLIFULogin(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = _("OpenLIFU Login")  # TODO: make this more human readable by adding spaces
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "OpenLIFU.OpenLIFU Modules")]
+        self.parent.dependencies = []  # add here list of module names that this module requires
+        self.parent.contributors = ["Andrew Howe (Kitware), Ebrahim Ebrahim (Kitware), Sadhana Ravikumar (Kitware), Peter Hollender (Openwater), Sam Horvath (Kitware), Brad Moore (Kitware)"]
+        # short description of the module and a link to online module documentation
+        # _() function marks text as translatable to other languages
+        self.parent.helpText = _(
+            "This is the login module of the OpenLIFU extension for focused ultrasound. "
+            "More information at <a href=\"https://github.com/OpenwaterHealth/SlicerOpenLIFU\">github.com/OpenwaterHealth/SlicerOpenLIFU</a>."
+        )
+        # organization, grant, and thanks
+        self.parent.acknowledgementText = _(
+            "This is part of Openwater's OpenLIFU, an open-source "
+            "hardware and software platform for Low Intensity Focused Ultrasound (LIFU) research "
+            "and development."
+        )
+
+#
+# OpenLIFULoginParameterNode
+#
+
+@parameterNodeWrapper
+class OpenLIFULoginParameterNode:
+    user_account_mode : bool
+
+#
+# OpenLIFULoginWidget
+#
+
+class OpenLIFULoginWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+    """Uses ScriptedLoadableModuleWidget base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent=None) -> None:
+        """Called when the user opens the module the first time and the widget is initialized."""
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        VTKObservationMixin.__init__(self)  # needed for parameter node observation
+        self.logic = None
+        self._parameterNode = None
+        self._parameterNodeGuiTag = None
+
+    def setup(self) -> None:
+        """Called when the user opens the module the first time and the widget is initialized."""
+        ScriptedLoadableModuleWidget.setup(self)
+
+        # Load widget from .ui file (created by Qt Designer).
+        # Additional widgets can be instantiated manually and added to self.layout.
+        uiWidget = slicer.util.loadUI(self.resourcePath("UI/OpenLIFULogin.ui"))
+        self.layout.addWidget(uiWidget)
+        self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+        # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+        # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+        # "setMRMLScene(vtkMRMLScene*)" slot.
+        uiWidget.setMRMLScene(slicer.mrmlScene)
+
+        # Create logic class. Logic implements all computations that should be possible to run
+        # in batch mode, without a graphical user interface.
+        self.logic = OpenLIFULoginLogic()
+
+        # Connections
+
+        # Buttons
+        self.ui.userAccountModePushButton.connect("clicked()", self.onUserAccountModeClicked)
+
+        # ====================================
+        
+        # Make sure parameter node is initialized (needed for module reload)
+        self.initializeParameterNode()
+
+        self.updateUserAccountModeButton()
+
+    def cleanup(self) -> None:
+        """Called when the application closes and the module widget is destroyed."""
+        self.removeObservers()
+
+    def enter(self) -> None:
+        """Called each time the user opens this module."""
+        # Make sure parameter node exists and observed
+        self.initializeParameterNode()
+
+    def exit(self) -> None:
+        """Called each time the user opens a different module."""
+        # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self._parameterNodeGuiTag = None
+
+    def onSceneStartClose(self, caller, event) -> None:
+        """Called just before the scene is closed."""
+        # Parameter node will be reset, do not use it anymore
+        self.setParameterNode(None)
+
+    def onSceneEndClose(self, caller, event) -> None:
+        """Called just after the scene is closed."""
+        # If this module is shown while the scene is closed then recreate a new parameter node immediately
+        if self.parent.isEntered:
+            self.initializeParameterNode()
+
+    def initializeParameterNode(self) -> None:
+        """Ensure parameter node exists and observed."""
+        # Parameter node stores all user choices in parameter values, node selections, etc.
+        # so that when the scene is saved and reloaded, these settings are restored.
+
+        self.setParameterNode(self.logic.getParameterNode())
+
+    def setParameterNode(self, inputParameterNode: Optional[OpenLIFULoginParameterNode]) -> None:
+        """
+        Set and observe parameter node.
+        Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+        """
+
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+
+        self._parameterNode = inputParameterNode
+        if self._parameterNode:
+            # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
+            # ui element that needs connection.
+            self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
+            self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.onParameterNodeModified)
+
+
+    def onUserAccountModeClicked(self):
+        new_user_account_mode_state = not self._parameterNode.user_account_mode
+        if new_user_account_mode_state:
+            self.logic.start_user_account_mode()
+        else:
+            set_user_account_mode_state(new_user_account_mode_state)
+
+    def updateUserAccountModeButton(self):
+        if self._parameterNode.user_account_mode:
+            self.ui.userAccountModePushButton.setText("Exit User Account Mode")
+        else:
+            self.ui.userAccountModePushButton.setText("Start User Account Mode")
+            self.ui.userAccountModePushButton.setToolTip(
+                    "User Account mode will enforce restrictions over available widgets based on user credentials."
+                )
+    
+    def onParameterNodeModified(self, caller, event) -> None:
+        self.updateUserAccountModeButton()
+
+# OpenLIFULoginLogic
+#
+
+class OpenLIFULoginLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget.
+    Uses ScriptedLoadableModuleLogic base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self) -> None:
+        """Called when the logic class is instantiated. Can be used for initializing member variables."""
+        ScriptedLoadableModuleLogic.__init__(self)
+
+    def getParameterNode(self):
+        return OpenLIFULoginParameterNode(super().getParameterNode())
+
+    def clear_session(self) -> None:
+        self.current_session = None
+
+    def start_user_account_mode(self):
+        set_user_account_mode_state(True)
+        slicer.util.selectModule("OpenLIFULogin")
