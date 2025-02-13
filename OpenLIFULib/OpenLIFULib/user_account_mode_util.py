6d5c2b2ace3e9e33ec38a6311ff9e28b5efa5673
Andrew Howe <arhowe00@gmail.com>
2025-02-13 09:35:06 -0500

    Get login parameter node via util convenience fn (#174)
    Co-authored-by: Sadhana Ravikumar <sadhana.ravikumar@kitware.com>

--- OpenLIFULib/OpenLIFULib/user_account_mode_util.py
+++ OpenLIFULib/OpenLIFULib/user_account_mode_util.py
@@ -1,11 +1,11 @@
-import slicer
+from OpenLIFULib.util import (
+        get_openlifu_login_parameter_node,
+        )
 
 def get_user_account_mode_state() -> bool:
     """Get user account mode state from the OpenLIFU Login module's parameter node"""
-    openlifu_login_parameter_node = slicer.util.getModuleLogic('OpenLIFULogin').getParameterNode()
-    return openlifu_login_parameter_node.user_account_mode
+    return get_openlifu_login_parameter_node().user_account_mode
 
 def set_user_account_mode_state(new_user_account_mode_state: bool):
     """Set user account mode state in OpenLIFU Login module's parameter node"""
-    openlifu_login_parameter_node = slicer.util.getModuleLogic('OpenLIFULogin').getParameterNode()
-    openlifu_login_parameter_node.user_account_mode = new_user_account_mode_state
+    get_openlifu_login_parameter_node().user_account_mode = new_user_account_mode_state
