3ee4696f4feb65f0964c58b99ab1f5ac46d71c3a
Sadhana Ravikumar <sadhana.ravikumar@kitware.com>
2025-03-13 15:51:02 -0400

    Handle transducer parent folder visibility (#199)

--- OpenLIFULib/OpenLIFULib/transducer_tracking_wizard_utils.py
+++ OpenLIFULib/OpenLIFULib/transducer_tracking_wizard_utils.py
@@ -95,10 +95,8 @@ def get_threeD_transducer_tracking_view_node():
 def hide_displayable_nodes_from_view(wizard_view_nodes: List[vtkMRMLViewNode]):
 
     # IDs of all the view nodes in the main Window. This excludes the photoscan's view node
-    views_mainwindow = [
-        node.GetID() for node in slicer.util.getNodesByClass('vtkMRMLViewNode') 
-        for wizard_view_node in wizard_view_nodes
-        if node.GetID() != wizard_view_node.GetID()]
+    all_view_nodes = slicer.util.getNodesByClass('vtkMRMLViewNode')
+    views_mainwindow = [node.GetID() for node in all_view_nodes if node not in wizard_view_nodes]
     
     # Set the view nodes for all displayable nodes.
     # If GetViewNodeIDs() is (), the node is displayed in all views so we need to exclude the photoscan view
