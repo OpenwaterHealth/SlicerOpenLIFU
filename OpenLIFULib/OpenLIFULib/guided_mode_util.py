7999e82cc711a94e133e83205fa1a522f23291f3
Andrew Howe <arhowe00@gmail.com>
2025-04-08 12:00:04 -0400

    Enforce guided mode visibility for static ui content (#128)
    This commit creates a mechanism for hiding static content in the ui
    file. Widgets can have a dynamic property called
    `slicer.openlifu.hide-in-guided-mode` which, **when it exists at all as
    a property for the widget, regardless of what the value is**, the widget
    will be hidden when guided mode is activated. Some widgets in the ui
    were given this property, and the Workflow object in the home module
    will loop over all the modules, find the widgets that have that
    property, and then hide them.
    
    Currently, this does not deal with the case for more complex widgets
    that may dynamically choose which widgets to hide. Such examples are the
    OpenLIFUAlgorithmInputWidget, which dynamically populates widgets and
    may have to make a decision of which widgets to hide. There is currently
    a precedent set for this marked as TODO. The plan is to add those
    widgets into a list and then call a function that the widget has to
    define.

--- OpenLIFULib/OpenLIFULib/guided_mode_util.py
+++ OpenLIFULib/OpenLIFULib/guided_mode_util.py
@@ -235,6 +235,41 @@ class Workflow:
         for workflow_controls in self.workflow_controls.values():
             workflow_controls.update()
 
+    def enforceGuidedModeVisibility(self, enforced: bool = False):
+
+        # ---- Locate widgets ----
+
+        # First, find all widgets that have the dynamic property set in the .ui
+        # file
+        hide_in_guided_mode_widgets = []
+        for moduleName in self.modules:
+            module = slicer.util.getModule(moduleName)
+            widgetRepresentation = module.widgetRepresentation()
+            all_widgets = slicer.util.findChildren(widgetRepresentation)
+            for widget in all_widgets:
+                hide_in_guided_mode = widget.property("slicer.openlifu.hide-in-guided-mode")
+                # If the widget does not have the specified property,
+                # QWidget.property("property-name") returns QVariant()—an
+                # invalid QVariant equivalent to None in Python.
+                if hide_in_guided_mode is not None:
+                    hide_in_guided_mode_widgets.append(widget)
+
+        # Second, find all widgets that we want to call the function to hide in
+        # guided mode, usually more complex widgets
+        complex_widgets = []
+        # TODO
+            
+        # ---- Hide ----
+
+        for widget in hide_in_guided_mode_widgets:
+            widget.hide() if enforced else widget.show()
+
+        for widget in complex_widgets:
+            widget.enforceGuidedModeVisibility(enforced)
+
+        return
+
+
 class GuidedWorkflowMixin:
     """A mixin class to add guided mode workflow related methods to a ScriptedLoadableModuleWidget"""
 
