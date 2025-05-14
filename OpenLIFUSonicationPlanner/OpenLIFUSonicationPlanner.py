OpenwaterHealth/SlicerOpenLIFU

commit 98652ea70bbaefeafe7f4899f64796ec47a8d6eb

Replace exception handling with display_errors decorator (#317)

OpenLIFUSonicationPlanner/OpenLIFUSonicationPlanner.py
@@ -35,6 +35,7 @@ from OpenLIFULib.guided_mode_util import GuidedWorkflowMixin
 from OpenLIFULib.user_account_mode_util import UserAccountBanner
 from OpenLIFULib.util import (
     create_noneditable_QStandardItem,
+    display_errors,
     replace_widget,
 )
 
@@ -309,6 +310,7 @@ class OpenLIFUSonicationPlannerWidget(ScriptedLoadableModuleWidget, VTKObservati
     def onTargetNameModified(self, caller, event):
         self.updateInputOptions()
 
+    @display_errors
     def onComputeSolutionClicked(self):
         activeData = self.algorithm_input_widget.get_current_data()
 
@@ -322,8 +324,6 @@ class OpenLIFUSonicationPlannerWidget(ScriptedLoadableModuleWidget, VTKObservati
                 slicer.app.processEvents()
                 self.logic.computeSolution(activeData["Volume"], activeData["Target"],
                                            activeData["Transducer"], activeData["Protocol"])
-            except Exception as e:
-                slicer.util.errorDisplay(f"Solution computation failed due to the following reason:\n{e}")
             finally:
                 self.updateSolutionProgressBar()
 

commit 131faab5ae96a9b83b1040aab79cb0c63e02ff6f

Fix compute solution button (#317)
The button to compute solution was broken in
98652ea70bbaefeafe7f4899f64796ec47a8d6eb

OpenLIFUSonicationPlanner/OpenLIFUSonicationPlanner.py
@@ -311,7 +311,7 @@ class OpenLIFUSonicationPlannerWidget(ScriptedLoadableModuleWidget, VTKObservati
         self.updateInputOptions()
 
     @display_errors
-    def onComputeSolutionClicked(self):
+    def onComputeSolutionClicked(self, checked:bool):
         activeData = self.algorithm_input_widget.get_current_data()
 
         # In case a PNP was previously being displayed, hide it since it is about to no longer belong to the active solution.