fc1f7132955ce9a9bcb204fa41d0380ee6ca63d2
Andrew Howe <arhowe00@gmail.com>
2025-05-28 12:01:26 -0400

    Make adding new subjects update experimental selector (#369)

--- OpenLIFUData/OpenLIFUData.py
+++ OpenLIFUData/OpenLIFUData.py
@@ -1031,6 +1031,7 @@ class OpenLIFUDataWidget(ScriptedLoadableModuleWidget, VTKObservationMixin, Guid
 
                 #Update loaded subjects view
                 self.updateSubjectSessionSelectorFromDb(get_cur_db())
+                self.updateExperimentalSubjectSelectorFromDb(get_cur_db())
 
     @display_errors
     def getSubjectSessionAtIndex(self, index: qt.QModelIndex) -> Tuple[str, str]:
