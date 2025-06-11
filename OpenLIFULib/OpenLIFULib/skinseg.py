4f7bdacdd6e56f30aa4e006f60d78f6589fbd9c7
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2025-06-10 21:57:54 -0400

    Split skin mesh computation into steps
    This is part of #399 -- we split into steps so that we can
    use the foreground mask separately to fulfill #387

--- OpenLIFULib/OpenLIFULib/skinseg.py
+++ OpenLIFULib/OpenLIFULib/skinseg.py
@@ -13,9 +13,11 @@ def generate_skin_segmentation(volume_node:vtkMRMLScalarVolumeNode) -> vtkMRMLMo
     """
     volume_array = slicer.util.arrayFromVolume(volume_node).transpose((2,1,0)) # the array indices come in KJI rather than IJK so we permute them
     volume_affine_RAS = get_IJK2RAS(volume_node)
-    skin_mesh = openlifu_lz().virtual_fit.compute_skin_mesh_from_volume(
-        volume_array = volume_array,
-        volume_affine_RAS = volume_affine_RAS)
+
+    foreground_mask_array = openlifu_lz().seg.skinseg.compute_foreground_mask(volume_array)
+    foreground_mask_vtk_image = openlifu_lz().seg.skinseg.vtk_img_from_array_and_affine(foreground_mask_array, volume_affine_RAS)
+    skin_mesh = openlifu_lz().seg.skinseg.create_closed_surface_from_labelmap(foreground_mask_vtk_image)
+
     skin_mesh_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode")
     skin_mesh_node.SetAndObservePolyData(skin_mesh)
     
