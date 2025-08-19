b4825b915c28787458d7a0a5743703236ad6cf4b
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2025-08-19 08:55:11 -0400

    Speed up virtual fitting (#31)
    - Incorporate embree speedups from the work on
      https://github.com/OpenwaterHealth/OpenLIFU-python/issues/367
    - Cap BLAS threads, which seems to help the  pos search when not
      using embree. I tried this on ubuntu 22.04.

--- OpenLIFUPrePlanning/OpenLIFUPrePlanning.py
+++ OpenLIFUPrePlanning/OpenLIFUPrePlanning.py
@@ -29,6 +29,7 @@ from OpenLIFULib import (
     get_openlifu_data_parameter_node,
     get_target_candidates,
     openlifu_lz,
+    threadpoolctl_lz,
 )
 from OpenLIFULib.coordinate_system_utils import get_IJK2RAS
 from OpenLIFULib.events import SlicerOpenLIFUEvents
@@ -1109,15 +1110,21 @@ class OpenLIFUPrePlanningLogic(ScriptedLoadableModuleLogic):
         if skin_mesh_node is None:
             skin_mesh_node = generate_skin_segmentation(volume)
 
-        vf_transforms = openlifu_lz().run_virtual_fit(
-            units = units,
-            target_RAS = target.GetNthControlPointPosition(0),
-            standoff_transform = transducer_openlifu.get_standoff_transform_in_units(units),
-            options = protocol_openlifu.virtual_fit_options,
-            skin_mesh = skin_mesh_node.GetPolyData(),
-            progress_callback = progress_callback,
-            include_debug_info = include_debug_info,
-        )
+        with threadpoolctl_lz().threadpool_limits(limits=1): # caps BLAS and OpenMP threads
+            # Capping BLAS threads appears to have a performance improvement when running this algorithm in Slicer.
+            # This may be because Slicer already occupies BLAS threads with its VTK/ITK stuff and so the virtual fit's many
+            # tiny svd calls end up having more overhead than is worth it.
+            # For some unknown reason, the improvement is only noticable when we do not use the embree
+            # option in virtual fitting, which makes things very fast.
+            vf_transforms = openlifu_lz().run_virtual_fit(
+                units = units,
+                target_RAS = target.GetNthControlPointPosition(0),
+                standoff_transform = transducer_openlifu.get_standoff_transform_in_units(units),
+                options = protocol_openlifu.virtual_fit_options,
+                skin_mesh = skin_mesh_node.GetPolyData(),
+                progress_callback = progress_callback,
+                include_debug_info = include_debug_info,
+            )
         if include_debug_info:
             vf_transforms, debug_info = vf_transforms # In this case two things were actually returned, the first of which is the list of transforms
             self.load_vf_debugging_info(debug_info)
