1cd3e8d44a9ba28dd1815a35eec24247e903395f
Andrew Howe <andrew.howe@krs-3184.khq.kitware.com>
2025-08-25 13:22:42 -0400

    Refactor and set opacity thresholding in slice view (#433)

--- OpenLIFUSonicationPlanner/OpenLIFUSonicationPlanner.py
+++ OpenLIFUSonicationPlanner/OpenLIFUSonicationPlanner.py
@@ -354,7 +354,17 @@ class OpenLIFUSonicationPlannerWidget(ScriptedLoadableModuleWidget, VTKObservati
         self.ui.pnpColorSlider.blockSignals(False)
         self.ui.pnpOpacitySlider.blockSignals(False)
         self._updating_gui_from_sliders = False
-    
+
+        # Set up thresholding and following volume display node
+        pnp_volume_node.GetDisplayNode().SetAutoWindowLevel(0)
+        pnp_volume_node.GetDisplayNode().SetApplyThreshold(1)
+
+        vrDisplayNode = slicer.modules.volumerendering.logic().GetFirstVolumeRenderingDisplayNode(pnp_volume_node)
+        if vrDisplayNode is not None and not vrDisplayNode.GetFollowVolumeDisplayNode():
+          vrDisplayNode.SetFollowVolumeDisplayNode(1)
+        if vrDisplayNode is not None and vrDisplayNode.GetIgnoreVolumeDisplayNodeThreshold():
+          vrDisplayNode.SetIgnoreVolumeDisplayNodeThreshold(0)
+
         # Manually trigger updates to apply the new default values.
         self.onPnpColorSliderChanged(self.ui.pnpColorSlider.minimumValue, self.ui.pnpColorSlider.maximumValue)
         self.onPnpOpacitySliderChanged(self.ui.pnpOpacitySlider.value)
@@ -421,12 +431,7 @@ class OpenLIFUSonicationPlannerWidget(ScriptedLoadableModuleWidget, VTKObservati
         """Called when the PNP color slider values are changed."""
         data_parameter_node = get_openlifu_data_parameter_node()
         pnp_volume_node: "vtkMRMLScalarVolumeNode" = data_parameter_node.loaded_solution.pnp
-        pnp_volume_node.GetDisplayNode().AutoWindowLevelOff()
         pnp_volume_node.GetDisplayNode().SetWindowLevelMinMax(new_min_val, new_max_val)
-
-        vrDisplayNode = slicer.modules.volumerendering.logic().GetFirstVolumeRenderingDisplayNode(pnp_volume_node)
-        if vrDisplayNode is not None and not vrDisplayNode.GetFollowVolumeDisplayNode():
-          vrDisplayNode.SetFollowVolumeDisplayNode(1)
 
     def onPnpOpacitySliderChanged(self, new_min_val: float) -> None:
         """Called when the PNP opacity slider value is changed."""
@@ -434,9 +439,6 @@ class OpenLIFUSonicationPlannerWidget(ScriptedLoadableModuleWidget, VTKObservati
         pnp_volume_node: "vtkMRMLScalarVolumeNode" = data_parameter_node.loaded_solution.pnp
         pnp_volume_node.GetDisplayNode().SetThreshold(new_min_val, self.ui.pnpOpacitySlider.maximum)
 
-        vrDisplayNode = slicer.modules.volumerendering.logic().GetFirstVolumeRenderingDisplayNode(pnp_volume_node)
-        if vrDisplayNode is not None and not vrDisplayNode.GetFollowVolumeDisplayNode():
-          vrDisplayNode.SetFollowVolumeDisplayNode(1)
 
     def deleteSolutionAndSolutionAnalysisIfAny(self, reason:str):
         """Delete the solution in the data module and the solution analysis in
